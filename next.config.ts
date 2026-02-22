d50a98015db08214a375fd2cd86ae2a4be6a8b8c
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2026-02-22 21:31:19 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
